784cc2aa6ae6830ea1edb7173ea4d789efae549a
Stephanie Castro <stephanie.castrosilva@gmail.com>
2021-08-24 22:09:35 -0300

    Commit

---
+++ pacote-download/Exercicios-desafios/exe046.py
@@ -0,0 +1,10 @@
+''' Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artificio, indo de 10
+até 0, com uma pausa de 1 segundo entre eles. '''
+
+from time import sleep
+
+for fogos in range(10, -1, -1):
+    print(fogos)
+    sleep(1)
+
+print('BUM! BUM! POOOW \*/')
---
+++ pacote-download/Exercicios-desafios/exe047.py
@@ -0,0 +1,12 @@
+'''Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50'''
+
+for contador in range(2, 51, 2):
+    print(contador, end=' ')
+print('\nACABOU!')
+
+'''
+for contador in range(1, 50):
+    if contador % 2 == 0:
+        print(contador)
+print('Acabou')
+'''
